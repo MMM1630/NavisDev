4c90c2af7b8b35e8e258d20e71637fc063df4fbe
MMM <nurtileksatinbaev1@example.com>
2025-01-31 18:26:08 +0600

    add changes

--- navis/views.py
+++ navis/views.py
@@ -1,29 +1,30 @@
 from rest_framework import status, generics
 from rest_framework.response import Response
-from rest_framework.views import APIView
-from navis.models import Video, Services, AboutUs, Tools, Projects, Reviews, Vacancy, Event, Gallery
+from itertools import groupby
+from navis.models import Video, Services, AboutUs, Tools, Projects, Reviews, Vacancy, Event, Gallery, Consultation
 from navis.serializers import ServicesSerializers, ToolsSerializers, ProjectsSerializers, ReviewsSerializers, VacancySerializers, JobApplicationSerializers, \
     EventSerializers, GallerySerializers,VideoSerializers, AboutUsSerializers,ConsultationSerializer
 
 
-class ConsultationView(generics.GenericAPIView):
+class ConsultationView(generics.ListAPIView):
     serializer_class = ConsultationSerializer
+    queryset = Consultation.objects.all()
 
     def post(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         if serializer.is_valid():
-            serializer.save()  # Save the new object
+            serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-class ServicesView(APIView):
+class ServicesView(generics.ListAPIView):
     queryset = Services.objects.all()
     serializers_class = ServicesSerializers
 
-class AboutUsView(APIView):
+class AboutUsView(generics.ListAPIView):
     queryset = AboutUs.objects.all()
-    serializers_class = AboutUsSerializers
+    serializer_class = AboutUsSerializers
 
 class ToolsView(generics.ListAPIView):
     queryset = Tools.objects.all()
@@ -37,9 +38,23 @@ class ReviewsView(generics.ListAPIView):
     queryset = Reviews.objects.all()
     serializer_class = ReviewsSerializers
 
+
 class VacancyView(generics.ListAPIView):
-    queryset = Vacancy.objects.all()
     serializer_class = VacancySerializers
+
+
+    def list(self, request, *args, **kwargs):
+        queryset = Vacancy.objects.all()
+        sorted_vacancies = sorted(queryset, key=lambda x: x.job_title)
+        grouped_vacancies = groupby(sorted_vacancies, key=lambda x: x.job_title)
+
+        data = {}
+        for key, group in grouped_vacancies:
+            serialized_group = VacancySerializers(group, many=True).data
+            data[key] = serialized_group
+
+        return Response(data)
+
 
 class JobApplicationView(generics.GenericAPIView):
     serializer_class = JobApplicationSerializers
